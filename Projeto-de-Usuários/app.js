1aa9d38ae24c0203dccd816c6b7ef48ed01545ba
Lucas Afonso <lucasafonsoretiro7@gmail.com>
2025-03-19 16:21:31 -0300

    Commitando arquivo JS

--- Projeto-de-Usuários/app.js
+++ Projeto-de-Usuários/app.js
@@ -31,7 +31,7 @@ app.get('/users/:id', async (req, res) => {
     const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
     
     if (result.rows.length === 0) {
-        return res.status(404).json({ error: 'User  not found' });
+        return res.status(404).json({ error: 'Usuário não encontrado' });
     }
     
     res.json(result.rows[0]);
@@ -50,7 +50,7 @@ app.put('/users/:id', async (req, res) => {
         'UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4',
         [name, email, password, id]
     );
-    res.json({ message: 'User  updated' });
+    res.json({ message: 'Usuário atualizado' });
 });
 
 app.patch('/users/:id', async (req, res) => {
@@ -63,10 +63,10 @@ app.patch('/users/:id', async (req, res) => {
     const setString = keys.map((key, index) => `${key} = $${index + 1}`).join(', ');
     
     await pool.query(`UPDATE users SET ${setString} WHERE id = $${keys.length + 1}`, [...values, id]);
-    res.json({ message: 'User  updated' });
+    res.json({ message: 'Usuário atualizado' });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
