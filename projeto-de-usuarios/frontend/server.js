63647f3f22549799da8d04398c2543c347fc200f
Lucas Afonso <lucasafonsoretiro7@gmail.com>
2025-04-11 13:45:14 -0300

    Adicionando botão de editar nome e ajuste em arquivos relacionados Adicionado botão de edição de nome do usuário. O botão direciona para a rota de edição do usuário, permitindo atualizar o nome cadastrado. Também troquei o nome dos arquivos .ejs.

--- projeto-de-usuarios/frontend/server.js
+++ projeto-de-usuarios/frontend/server.js
@@ -12,22 +12,26 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/Lista-de-usuarios", async (req, res) => {
   const response = await fetch("http://localhost:3000/users");
   const users = await response.json();
-  res.render("users", { users });
+  res.render("lista-usuarios", { users });
 });
 
 app.get("/criar-usuario", (req, res) => {
-  res.render("novo-user", { users: [] });
+  res.render("form-usuario", { usuario: null });
 });
+
 
 app.post("/novo-usuario", async (req, res) => {
   const { name, email, password } = req.body;
 
-  fetch("http://localhost:3000/users", {
+  await fetch("http://localhost:3000/users", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, email, password }),
@@ -44,6 +48,29 @@ app.delete('/users/:id', async (req, res) => {
   });
 
   res.sendStatus(resposta.status);
+});
+
+app.get("/editar-nome/:id", async (req, res) => {
+  const { id } = req.params;
+
+    const resposta = await fetch(`http://localhost:3000/users/${id}`);
+    const usuario = await resposta.json();
+
+    res.render("form-usuario", { usuario });
+  
+});
+
+app.patch("/editar-usuario/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  await fetch(`http://localhost:3000/users/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name }),
+  });
+
+  res.sendStatus(200);
 });
 
 app.listen(4000, () => {
