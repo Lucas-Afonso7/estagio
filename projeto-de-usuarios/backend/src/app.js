31c56b5912b174e7f8e271d6b9962ba1272eb5e0
Lucas Afonso <lucasafonsoretiro7@gmail.com>
2025-04-16 18:42:02 -0300

    Adiciona package.json

--- projeto-de-usuarios/backend/src/app.js
+++ projeto-de-usuarios/backend/src/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'database',
     database: 'api_teste',
     password: '852',
     port: 5432,
