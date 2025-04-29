18a9c02790dcadd8979bc028daf3c088c9a81709
Lucas Afonso <lucasafonsoretiro7@gmail.com>
2025-04-29 00:32:25 -0300

    Adiciona lista de usuários com botões de editar senha, editar nome e deletar
    Implementado funcionalidades de edição de senha, edição de nome usando o mesmo formulário de cadastro, e exclusão de usuários.

--- projeto-de-usuarios/frontend-react/src/pages/Lista/index.jsx
+++ projeto-de-usuarios/frontend-react/src/pages/Lista/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Lista() {
   const [usuarios, setUsuarios] = useState([]);
@@ -7,19 +8,29 @@ function Lista() {
     fetch("http://localhost:3000/users")
       .then((response) => response.json())
       .then((data) => setUsuarios(data));
-  });
+  }, []);
+
+  async function deletarUsuario(id) {
+    await fetch(`http://localhost:3000/users/${id}`, {
+      method: "DELETE",
+    });
+
+    setUsuarios(usuarios.filter((usuario) => usuario.id !== id));
+  }
 
   return (
     <div>
       <h1>Lista de Usuários</h1>
-      <ul>
-        {usuarios.map((usuario) => (
-          <li>
-            <strong>Nome:</strong> {usuario.name} <br />
-            <strong>Email:</strong> {usuario.email}
-          </li>
-        ))}
-      </ul>
+      {usuarios.map((usuario) => (
+        <div key={usuario.id}>
+          <strong>Nome:</strong> {usuario.name} <br />
+          <strong>Email:</strong> {usuario.email} <br />
+          <Link to={`/EditarSenha/${usuario.id}`}>Editar Senha</Link>
+          <Link to={`/Cadastro/${usuario.id}`}>Editar Nome</Link>
+          <button onClick={() => deletarUsuario(usuario.id)}>Deletar</button>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
