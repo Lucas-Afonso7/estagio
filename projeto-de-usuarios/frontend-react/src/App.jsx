c55fe4d329ccafa61de06840361714324f15c9e5
Lucas Afonso <lucasafonsoretiro7@gmail.com>
2025-04-29 18:26:31 -0300

    Adiciona rota de editar senha e cadastro por id

--- projeto-de-usuarios/frontend-react/src/App.jsx
+++ projeto-de-usuarios/frontend-react/src/App.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Lista from "./pages/Lista";
+import EditarSenha from "./pages/Lista/EditarSenha.jsx";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <nav className="bg-gray-800 p-8">
-          <ul>
+        <nav className="bg-gray-800 p-4">
+          <ul className="flex space-x-4">
             <li>
-              <Link to="/cadastro">Cadastro</Link>
+              <Link to="/cadastro" className="text-white hover:text-gray-300">
+                Cadastro
+              </Link>
             </li>
             <li>
-              <Link to="/lista">Lista</Link>
+              <Link to="/lista" className="text-white hover:text-gray-300">
+                Lista
+              </Link>
             </li>
           </ul>
         </nav>
 
         <Routes>
+          <Route path="/" element={<Cadastro />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/cadastro/:id" element={<Cadastro />} />
           <Route path="/lista" element={<Lista />} />
+          <Route path="/EditarSenha/:id" element={<EditarSenha />} />
         </Routes>
       </div>
     </Router>
